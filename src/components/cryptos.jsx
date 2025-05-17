827b1007676a4220977f4229b147d96fc269f71a
Manan <aggarwalmanan18@gmail.com>
2025-05-18 00:21:18 +0530

    18/5

--- src/components/cryptos.jsx
+++ src/components/cryptos.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const cryptos = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="flex w-full bordr">
+        <div className="flex-1/2 ml-15 bordr p-10 gap-3">
+          <div className='flex flex-col gap-5'>
+            <h1 className='text-4xl font-semibold'>All Cryptos, One Platform</h1>
+            <p className='text-gray-400 text-lg'>Buy, sell, and convert all major cryptocurrencies on a single platform. A seamless experience with no compromises.</p>
+          </div>
+          <div className='h-[30vh] '></div>
+          <div className='flex gap-5 items-center py-5'>
+            <div className='text-2xl font-semibold'>Buy crypto now </div>
+            <i class="fi fi-br-arrow-up-right text-xl"></i>
+          </div>
+        </div>
+        <div className="flex-1/2 mr-15 bordr"></div>
+      </div>
+    </>
   )
 }
 
--- src/components/whycryptix.jsx
+++ src/components/whycryptix.jsx
@@ -8,23 +8,23 @@ const whycryptix = () => {
       <h1 className='centre text-4xl my-3'>Why Choose Cryptix?</h1>
       <p className='text-gray-400 text-2xl centre mb-10'>Benefits designed to provide a seamless, secure, and accessible experience for all users.</p>
       <div className='w-full h-fit centre bordr p-0'>
-        <div className="flex bordr mx-15 my-0">
-          <div className="flex flex-col gap-3 bordr">
+        <div className="flex bordr mx-15">
+          <div className="flex flex-col gap-3 bordr p-3">
             <div className='border centre'><i class="fi fi-rs-shield text-xl"></i></div>
             <h1 className='text-xl font-semibold'>Maximum Security</h1>
             <p className='text-gray-400'>Your assets are protected with cutting-edge security protocols.</p>
           </div>
-          <div className="flex flex-col gap-3 bordr">
+          <div className="flex flex-col gap-3 bordr p-3">
             <div className='border centre'><i class="fi fi-br-bolt text-xl"></i></div>
             <h1 className='text-xl font-semibold'>Instant Tansactions</h1>
             <p className='text-gray-400'>Execute your transactions in real-time, without delays.</p>
           </div>
-          <div className="flex flex-col gap-3 bordr">
+          <div className="flex flex-col gap-3 bordr p-3">
             <div className='border centre'><i class="fi fi-br-down-left-and-up-right-to-center text-xl"></i></div>
             <h1 className='text-xl font-semibold'>Optimized Fees</h1>
             <p className='text-gray-400'>Benefit from some of the lowest fees on the market.</p>
           </div>
-          <div className="flex flex-col gap-3 bordr">
+          <div className="flex flex-col gap-3 bordr p-3">
             <div className='border centre'><i class="fi fi-ts-screen text-xl"></i></div>
             <h1 className='text-xl font-semibold'>Premium Interface</h1>
             <p className='text-gray-400'>An elegant, intuitive design that’s easy to use, even for beginners.</p>
@@ -32,7 +32,7 @@ const whycryptix = () => {
         </div>
       
       </div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, provident omnis? Quaerat voluptates aspernatur soluta laudantium error commodi quas alias illo, vitae dolores nam sed, qui maiores consectetur enim voluptate nihil. Facilis autem numquam recusandae ullam ipsum voluptates saepe. Laborum dignissimos ratione optio illo obcaecati repellat iure corporis maxime eius natus voluptatum, voluptatibus reprehenderit ullam corrupti aspernatur dicta sit repellendus sapiente suscipit error. Perspiciatis, consequatur minima illo nisi odit omnis eum alias atque dolorum nostrum, maxime consectetur veniam ratione adipisci blanditiis, dignissimos voluptatem autem iusto molestiae id expedita ut.</div>
+        <div className='bordr h-[30vh] mx-15 centre'></div>
     </>
   )
 }
