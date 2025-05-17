2125a9d30c71bfdaaffd48220b6c0a82486b5cd7
Manan <aggarwalmanan18@gmail.com>
2025-05-17 18:01:19 +0530

    17/5

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
                         <div className="centre flex-col gap-3">
                             <div className='centre flex-col gap-10 text-center'>
                                 <div className="text-7xl centre font-semibold">
-                                    TakeControlOf <br />YourDigitalAssets
+                                    Take Control Of <br />Your Digital Assets
                                 </div>
                                 <div>
                                     Cryptix offers a seamless, secure experience for managing your digital assets. Instant <br/>transactions, optimized fees, and premium design.
