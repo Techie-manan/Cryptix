1b50cd70218e56cbebaf2fcd5211bd05423885a4
Manan <aggarwalmanan18@gmail.com>
2025-05-20 22:33:09 +0530

    20/5

--- src/components/cryptos.jsx
+++ src/components/cryptos.jsx
@@ -7,7 +7,7 @@ const cryptos = () => {
         <div className="flex-1/2 ml-15 bordr p-10 gap-3">
           <div className='flex flex-col gap-5'>
             <h1 className='text-4xl font-semibold'>All Cryptos, One Platform</h1>
-            <p className='text-gray-400 text-lg'>Buy, sell, and convert all major cryptocurrencies on a single platform. A seamless experience with no compromises.</p>
+            <p className='text-gray-400 text-lg'>Buy, sell, and convert allmajorcryptocurrencies on a single platform. A seamless experience with no compromises.</p>
           </div>
           <div className='h-[30vh] '></div>
           <div className='flex gap-5 items-center py-5'>
